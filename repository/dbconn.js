7acc25c6c2341d5c9311ee849385f809ca0e8cb0
haxul <s.s.starodubov@yandex.ru>
2022-05-22 15:29:19 +0400

    create database

--- repository/dbconn.js
+++ repository/dbconn.js
@@ -1,4 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const path = require('path');
+const os = require('os')
+
 
 const wordTableSql = `
 CREATE TABLE IF NOT EXISTS word (
@@ -12,7 +16,10 @@ CREATE TABLE IF NOT EXISTS word (
 class Db {
     conn = null
     connect() {
-        this.conn = new sqlite3.Database("database.sqlite", (err) => {
+        const dir = os.homedir()  
+        const flashPath = path.join(dir, ".flashcards")
+        if (!fs.existsSync(flashPath)) fs.mkdirSync(flashPath)
+        this.conn = new sqlite3.Database(path.join(flashPath, "database.sqlite"), (err) => {
             if (err) throw "cannot access to database"
             console.info("database is connected")
             this.conn.run(wordTableSql)
