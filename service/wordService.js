13f55db387238a3f130f3446301f3380c89d86c3
haxul <s.s.starodubov@yandex.ru>
2022-05-22 22:47:57 +0400

    feat: add Trie to find words by prefix

--- service/wordService.js
+++ service/wordService.js
@@ -1,11 +1,12 @@
-const { saveWordPersistant, fetchWordsPriorityOrder } = require("../repository/wordRepository")
+const {saveWordPersistant, fetchWordsPriorityOrder} = require("../repository/wordRepository")
 
 
 const saveWordHandler = (fn, event, word) => (event, word) => fn(word)
 const saveWordPersistHandler = saveWordHandler(saveWordPersistant)
 
 const startLessonHandler = (frame, event) =>
-    event => fetchWordsPriorityOrder(fetchedWords => frame.window.webContents.send("main::page::words::lesson", fetchedWords))
+    event => fetchWordsPriorityOrder(fetchedWords =>
+        frame.window.webContents.send("main::page::words::lesson", fetchedWords))
 
 
 module.exports = {
---
+++ util/trie.js
@@ -0,0 +1,72 @@
+/*
+prefix tree to find words by prefix
+
+ */
+
+class Trie {
+    _root = {"$": false}
+
+    addAll(words) {
+        for (let i = 0; i < words.length; i++) {
+            this.add(words[i])
+        }
+    }
+
+    add(word) {
+        let cur = this._root
+        for (let i = 0; i < word.length; i++) {
+            const ch = word[i]
+            if (ch === "$") throw new Error("the word cannot contain $ symbol")
+            if (!(ch in cur)) {
+                cur[ch] = {"$": false}
+            }
+            cur = cur[ch]
+        }
+
+        cur["$"] = true
+    }
+
+    findWordsWithPrefix(prefix) {
+        const node = this._findNodeWithPrefix(prefix)
+        if (!node) return []
+
+        return this._collectWordsFromNode(node, prefix)
+    }
+
+    _collectWordsFromNode(node, cur, arr = []) {
+        if (!node) return arr
+
+        if (Object.keys(node) === 0) return arr
+
+        if (node["$"]) arr.push(cur)
+
+        if (Object.keys(node).length === 1) {
+            return arr
+        }
+
+        for (let letter in node) {
+            if (letter === "$") continue
+            const nxtNode = node[letter]
+            this._collectWordsFromNode(nxtNode, cur + letter, arr)
+        }
+
+        return arr
+    }
+
+    _findNodeWithPrefix(prefix) {
+        if (!prefix) return null
+
+        let cur = this._root
+
+        for (let i = 0; i < prefix.length; i++) {
+            const ch = prefix[i]
+            if (ch in cur) {
+                cur = cur[ch]
+            } else return null
+        }
+
+        return cur
+    }
+}
+
+module.exports = {Trie}
