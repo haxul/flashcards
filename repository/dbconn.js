6513da375ec34a348dbf7502cc0689bef66200cf
haxul <s.s.starodubov@yandex.ru>
2022-05-22 12:46:27 +0400

    sql create table

--- repository/dbconn.js
+++ repository/dbconn.js
@@ -1,11 +1,21 @@
 const sqlite3 = require('sqlite3').verbose();
 
+const wordTableSql = `
+CREATE TABLE IF NOT EXISTS word (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    english varchar(500) NOT NULL,
+    russian varchar(500) NOT NULL,
+    priority int NOT NULL,
+    last_updated timestamp NOT NULL
+    )
+`
 class Db {
     conn = null
     connect() {
         this.conn = new sqlite3.Database("database.sqlite", (err) => {
             if (err) throw "cannot access to database"
             console.info("database is connected")
+            this.conn.run(wordTableSql)
         })
     }
 }
